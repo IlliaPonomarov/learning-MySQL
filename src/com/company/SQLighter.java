b1badc292f7cf49060818ad600c0091858f1a337
IlliaPonomarov <71827930+IlliaPonomarov@users.noreply.github.com>
2021-09-13 16:28:27 +0300

    Update SQLighter.java

--- src/com/company/SQLighter.java
+++ src/com/company/SQLighter.java
@@ -1,21 +1,81 @@
 package com.company;
-import java.sql.*;
-import java.util.*;
 
+import java.io.File;
+import java.sql.*;
+import java.lang.*;
 
 public class SQLighter {
 
-    private Connection connection;
-    private static final String CON_STR = "jdbc:sqlite:C:/Users/hp/IdeaProjects/untitled3/src/com/company/sqliteDB.db";
-
     private static SQLighter instance;
+    Connection connection;
+    Statement  statement;
+    ResultSet resultSet;
 
-    public  static  synchronized SQLighter getInstance() throws SQLException{
+
+    private SQLighter() {
+
+        File path_sqlte = new File("src\\com\\company");
+        File path_jdbc = new File("lib\\");
+
+        String url = "jdbc:sqlite:" + path_sqlte.getAbsolutePath() + "\\omg.db";
+        System.out.println(path_jdbc.getAbsolutePath());
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            System.out.println("Connecting to database ...");
+            connection = DriverManager.getConnection(url);
+            statement = connection.createStatement();
+            System.out.println("Connected");
+        }
+        catch (SQLException | ClassNotFoundException throwables){
+            throwables.printStackTrace();
+        }
+    }
+
+    public static SQLighter getInstance() {
         if (instance == null)
             instance = new SQLighter();
         return instance;
     }
 
+    public void output() throws SQLException{
+
+        String sql_requests = "SELECT * FROM Test";
+        resultSet = statement.executeQuery(sql_requests);
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("name"));
+            System.out.println(resultSet.getString("surname"));
+        }
+    }
+
+
+    public void insert() throws  SQLException{
+        String sql_requests ="INSERT INTO Test (name, surname) VALUES (?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql_requests);
+        preparedStatement.setString(1, "Illia");
+        preparedStatement.setString(2, "Ponomarov");
+
+        preparedStatement.executeUpdate();
+    }
+
+
+    public void delete() throws  SQLException{
+        String sql_requests = "DELETE FROM Test WHERE id=1";
+        int r = statement.executeUpdate(sql_requests);
+        System.out.printf("Delete %d\n", r);
+    }
+    
     
 
+    public void update() throws SQLException{
+        String sql_requests = "UPDATE Test SET name=\'IGOR\' WHERE id=1";
+        int r = statement.executeUpdate(sql_requests);
+        System.out.println();
+    }
+
+
+
+
+
 }
+
